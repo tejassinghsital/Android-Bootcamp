ce4ada50246fba68737a735e1b65da7d2ba94592
Tejas <tejassinghsital@gmail.com>
2022-01-30 00:06:55 +0530

    Android life cycle methods working and unpadting them using super

--- Android basics/AndroidLifeCycle/app/src/main/java/com/tejas/androidlifecycle/MainActivity.java
+++ Android basics/AndroidLifeCycle/app/src/main/java/com/tejas/androidlifecycle/MainActivity.java
@@ -2,7 +2,11 @@ package com.tejas.androidlifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+
+        TextView firstText = findViewById(R.id.tvFirstAcitvity);
+
+
+        firstText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent myIntent =new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this ,"onStart",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this ,"onResume",Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this ,"onPause",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this ,"onStop",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this ,"onDestroy",Toast.LENGTH_SHORT).show();
     }
 }
--- Android basics/AndroidLifeCycle/app/src/main/java/com/tejas/androidlifecycle/SecondActivity.java
+++ Android basics/AndroidLifeCycle/app/src/main/java/com/tejas/androidlifecycle/SecondActivity.java
@@ -2,7 +2,13 @@ package com.tejas.androidlifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +16,57 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    //No need of below line because we don't want to go anywhere from SecondActivity text view
+        //TextView secondText= findViewById(R.id.tvSecondActivity);
+
+        Toast toast= Toast.makeText(this,"SecondOnCreateJatin",Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL,0,0);//not works after api level 30 that's why not working on emulator but in 3s prime 
+        toast.show();
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        Toast toast=Toast.makeText(this,"SecondOnStart",Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        Toast myToast3=Toast.makeText(this,"SecondOnResume",Toast.LENGTH_SHORT);
+        myToast3.setGravity(Gravity.CENTER,0,0);
+        myToast3.show();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        Toast myToast4=Toast.makeText(this,"SecondOnPause",Toast.LENGTH_SHORT);
+        myToast4.setGravity(Gravity.CENTER,0,0);
+        myToast4.show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        Toast myToast5=Toast.makeText(this,"SecondOnStop",Toast.LENGTH_SHORT);
+        myToast5.setGravity(Gravity.CENTER,0,0);
+        myToast5.show();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        Toast myToast6=Toast.makeText(this,"SecondOnDestroy",Toast.LENGTH_SHORT);
+        myToast6.setGravity(Gravity.CENTER,0,0);
+        myToast6.show();
     }
 }
